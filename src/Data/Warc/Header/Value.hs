60685ba045a5502629a386c68b017cf5c4deed7d
John Haynes <john.andrew.haynes@gmail.com>
2016-08-05 20:20:52 +1000

    type the Date properly

--- src/Data/Warc/Header/Value.hs
+++ src/Data/Warc/Header/Value.hs
@@ -2,29 +2,51 @@
 
 module Data.Warc.Header.Value where
 
-import Data.Attoparsec.ByteString.Char8      (Parser, choice)
+import Data.Attoparsec.ByteString.Char8      (Parser, choice, char, takeWhile1)
 import Data.Char                             (isSpace)
 import Data.ByteString.Char8                 (ByteString, readInt, pack)
 
 import Data.Warc.Header.Key
 import Data.Warc.Shared
 
+data Date = Date
+           { year :: Int
+           , month :: Int 
+           , day :: Int
+           , hour :: Int
+           , min :: Int
+           , sec :: Int } deriving (Eq, Show)
+
 data Value = IntValue Int
            | CompressionModeValue CompressionMode
+           | DateValue Date
            | StringValue ByteString deriving (Eq, Show)
 
 value :: Key -> Parser Value
-value (MandatoryKey ContentLength) = IntValue <$> int
+value (MandatoryKey ContentLength) = IntValue <$> intThenSpace
+value (MandatoryKey WarcDate) = DateValue <$> date
 value (CustomKey CompressionMode) = CompressionModeValue <$> compressMode
-value (CustomKey UncompressedContentLength) = IntValue <$> int
+value (CustomKey UncompressedContentLength) = IntValue <$> intThenSpace
 value _ = StringValue <$> takeTill1 (\x -> x == '\r' || x == '\n')
 
-int :: Parser Int
-int = do
-    x <- takeTill1 isSpace
-    case readInt x of
-        Just (i,_) -> pure i
-        Nothing    -> fail "meh" -- TODO <- fix
+intThenSpace :: Parser Int
+intThenSpace = int =<< takeTill1 isSpace
+
+date :: Parser Date
+date = Date <$> intThenSkipChar '-'
+            <*> intThenSkipChar '-'
+            <*> intThenSkipChar 'T'
+            <*> intThenSkipChar ':'
+            <*> intThenSkipChar ':'
+            <*> intThenSkipChar 'Z'
+    where
+    intThenSkipChar :: Char -> Parser Int
+    intThenSkipChar cAfter = int =<< takeTill1 (==cAfter) <* takeWhile1 (==cAfter)
+
+int :: ByteString -> Parser Int
+int x = case readInt x of
+    Just (i,_) -> pure i
+    Nothing    -> fail ("Expected integer value" ++ show x)
 
 compressMode :: Parser CompressionMode
 compressMode = choice [ Compressed <%> "contentonly"
@@ -33,5 +55,7 @@ compressMode = choice [ Compressed <%> "contentonly"
 toByteString :: Value -> ByteString
 toByteString (CompressionModeValue Compressed) = "contentonly"
 toByteString (CompressionModeValue Uncompressed) = "none"
+toByteString (DateValue (Date yr mnt day hr mnu sec)) = let [a,b,c,d,e,f] = map show [yr, mnt, day, hr, mnu, sec]
+    in pack $ concat [a,"-",b,"-",c,"T",d,":",e,":",f,"Z"]
 toByteString (IntValue i) = pack . show $ i
 toByteString (StringValue bs) = bs
